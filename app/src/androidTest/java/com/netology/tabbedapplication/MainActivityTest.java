1b5e294682950b925689b061bbaa0c8f779f59fa
AlexanderTokar <alextokar@inbox.ru>
2023-02-27 20:56:30 +0300

    cleaning code commit

--- app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/netology/tabbedapplication/MainActivityTest.java
@@ -39,25 +39,16 @@ public class MainActivityTest {
     public void mainActivityTest() {
         ViewInteraction textView = onView(
                 allOf(withId(R.id.section_label), withText("Page: 1"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
                         isDisplayed()));
         textView.check(matches(withText("Page: 1")));
 
         ViewInteraction tabView = onView(
                 allOf(withContentDescription("Tab 2"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.tabs),
-                                        0),
-                                1),
                         isDisplayed()));
         tabView.perform(click());
 
         ViewInteraction textView2 = onView(
                 allOf(withId(R.id.section_label), withText("Page: 2"),
-                        withParent(allOf(withId(R.id.constraintLayout),
-                                withParent(withId(R.id.view_pager)))),
                         isDisplayed()));
         textView2.check(matches(withText("Page: 2")));
     }
